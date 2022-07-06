bdbe401f85fb27f08463c45cf50f65c17d0fe635
John Szabo <john.szabo.jr@gmail.com>
2022-07-06 10:12:41 +0200

    improve

--- 99_bottles.dart
+++ 99_bottles.dart
@@ -1,23 +1,15 @@
-String pluralize(int i) {
-  return i == 1 ? '' : 's';
-}
-
-String zeroify(int i) {
-  return i == 0 ? 'no more' : i.toString();
+String getBottles(int i) {
+  String n = i > 0 ? i.toString() : 'no more';
+  String s = i == 1 ? '' : 's';
+  return '$n bottle$s';
 }
 
 void main() {
-  for (var i = 99; i >= 0; i--) {
-    var line1 =
-        '${zeroify(i)} bottle${pluralize(i)} of beer on the wall, ${zeroify(i)} bottle${pluralize(i)} of beer.';
-    line1 = line1[0].toUpperCase() + line1.substring(1);
-    print(line1);
-    var line2 =
-        'Take one down and pass it around, ${zeroify(i - 1)} bottle${pluralize(i - 1)} of beer on the wall.';
-    if (i == 0) {
-      line2 =
-          'Go to the store and buy some more, 99 bottles of beer on the wall.';
-    }
-    print(line2);
+  String beer = 'of beer on the wall';
+  String l2var = 'Go to the store and buy some more, 99 bottles $beer.';
+  for (int i = 99; i >= 0; i--) {
+    String l1 = '${getBottles(i)} $beer, ${getBottles(i)} of beer.';
+    String l2 = 'Take one down and pass it around, ${getBottles(i - 1)} $beer.';
+    print('${l1[0].toUpperCase() + l1.substring(1)}\n${i > 0 ? l2 : l2var}');
   }
 }
